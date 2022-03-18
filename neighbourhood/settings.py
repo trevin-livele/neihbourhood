003ca2ee0a2186522d9be1253faff4d88e14c519
trevin-livele <liveletrevin6@gmail.com>
2022-03-18 22:33:37 +0300

    registered model in admin

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'neighbourhood.wsgi.application'
 
+AUTH_USER_MODEL = 'accounts.Account'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
