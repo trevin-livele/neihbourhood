1b2069300a036034b4e3e15533887acf2aaac085
trevin-livele <liveletrevin6@gmail.com>
2022-03-19 08:43:21 +0300

    added category app

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
     path('accounts/',include('accounts.urls')),
+    # path('category/',include('category.urls')),
+
 
 ] +static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
