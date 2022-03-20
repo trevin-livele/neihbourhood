0d70aa53c9994dde573392655e9c69d2dfdf4399
trevin-livele <liveletrevin6@gmail.com>
2022-03-20 16:14:27 +0300

    created models and relationships in app

--- app_main/models.py
+++ app_main/models.py
@@ -29,3 +29,53 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+
+# NeighbourHood Model
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    occupants_count = models.IntegerField(default=0)
+    admin = models.ForeignKey(Account, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+# business class model
+class Business(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+# contact class model
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50, blank=True, null=True)
+    phone = models.CharField(max_length=50)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.name
